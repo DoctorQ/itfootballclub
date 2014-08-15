4fd356bdf60b507219f844968552870a13b359bb
Hui Qian <542113578@qq.com>
2014-08-15 20:47:55 +0800

    Signed-off-by: Hui Qian <542113578@qq.com>

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -105,9 +105,9 @@ insert into a_club values (null,'Manchester City','1894-00-00','Etihad Stadium',
 insert into a_club values (null,'Tottenham Hotspur','1882-00-00','White Hart Lane','Villas Boas',null,'images/tottenhamhotspur.png','North London,England');
 insert into a_club values (null,'Aston Villa','1874-00-00','Villa Park','Paul Lambert',null,'images/avfc.png','Birmingham,England');
 insert into a_club values (null,'Everton','1878-01-01','Goodison Park','Roberto Martinez','','images/everton.png','Liverpool,England');
-insert into a_club values (null,'Cardiff City','1899-00-00','Cardiff City Stadium','Malky Mackay',null,'images/cardif.png','Cardiff,Wales');
+#insert into a_club values (null,'Cardiff City','1899-00-00','Cardiff City Stadium','Malky Mackay',null,'images/cardif.png','Cardiff,Wales');
 insert into a_club values (null,'Crystal Palace','1905-00-00','Selhurst Park','Ian Scott Holloway',null,'images/crystalpalace.png','South London,England');
-insert into a_club values (null,'Fulham','1879-00-00','Craven Cottage','Martin Jol',null,'images/ffc.png','London,England');
+#insert into a_club values (null,'Fulham','1879-00-00','Craven Cottage','Martin Jol',null,'images/ffc.png','London,England');
 insert into a_club values (null,'Sunderland Association','1879-00-00','Stadium of Light','Paolo Di Canio','','images/sunderland.png','Sunderland,England');
 insert into a_club values (null,'Southampton','1885-11-00','St. Mary''s Stadiu','Pochettino',null,'images/southampton.png','Southampton,England');
 insert into a_club values (null,'Newcastle United','1881-00-00','St James'' Park','Alan Pardew',null,'images/newcastleunited.png','Newcastle,England');
@@ -115,30 +115,11 @@ insert into a_club values (null,'West Ham United','1895-00-00','Upton Park','Sam
 insert into a_club values (null,'Stoke City','1868-00-00','The Britannia Stadium','Mark Hughes',null,'images/stokecity.png','Stoke-on-Trent,England');
 insert into a_club values (null,'West Bromwich Albion','1878-00-00','The Hawthorns','Steve Clarke','','images/westbromwich.png','West Bromwich,England');
 insert into a_club values (null,'Swansea City','1912-00-00','Liberty Stadium','Michael Laudrup',null,'images/swansea.png','Swansea,Wales');
-insert into a_club values (null,'Norwich City','1902-00-00','Carrow Road','Christopher "Chris" Hughton',null,'images/norwich.png','Norwich City,England');
+#insert into a_club values (null,'Norwich City','1902-00-00','Carrow Road','Christopher "Chris" Hughton',null,'images/norwich.png','Norwich City,England');
 insert into a_club values (null,'Hull City Association','1904-00-00','Kingston Communications Stadium','Bruce',null,'images/hullcity.png','Hull City,England');
-#向积分表中擦人数据
-insert into a_scoreboard values (null,'Manchester United',2,1,2,7,7,6,'images/manchesterunited.png');
-insert into a_scoreboard values (null,'Chelsea',3,1,1,10,6,2,'images/chelsea.png');
-insert into a_scoreboard values (null,'Arsenal',4,1,0,12,11,6,'images/arsenal.png');
-insert into a_scoreboard values (null,'Liverpool',3,1,1,10,5,3,'images/liverpool.png');
-insert into a_scoreboard values (null,'Manchester City',3,1,1,10,12,4,'images/mcfc.png');
-insert into a_scoreboard values (null,'Tottenham Hotspur',4,0,1,12,5,1,'images/tottenhamhotspur.png');
-insert into a_scoreboard values (null,'Aston Villa',2,3,0,6,6,6,'images/avfc.png');
-insert into a_scoreboard values (null,'Everton',2,3,0,9,6,4,'images/everton.png');
-insert into a_scoreboard values (null,'Cardiff City',1,2,2,5,4,4,'images/cardif.png');
-insert into a_scoreboard values (null,'Crystal Palace',1,3,1,4,3,6,'images/crystalpalace.png');
-insert into a_scoreboard values (null,'Fulham',1,3,1,4,3,7,'images/ffc.png');
-insert into a_scoreboard values (null,'Sunderland Association',0,4,1,1,3,11,'images/sunderland.png');
-insert into a_scoreboard values (null,'Southampton',2,1,2,8,3,2,'images/southampton.png');
-insert into a_scoreboard values (null,'Newcastle United',2,2,1,7,5,8,'images/newcastleunited.png');
-insert into a_scoreboard values (null,'West Ham United',1,2,2,5,4,4,'images/westhamunited.png');
-insert into a_scoreboard values (null,'Stoke City',2,2,1,7,4,5,'images/stokecity.png');
-insert into a_scoreboard values (null,'West Bromwich Albion',1,2,2,5,4,4,'images/westbromwich.png');
-insert into a_scoreboard values (null,'Swansea City',2,2,1,7,7,7,'images/swansea.png');
-insert into a_scoreboard values (null,'Norwich City',1,3,1,4,3,6,'images/norwich.png');
-insert into a_scoreboard values (null,'Hull City Association',2,2,1,7,5,7,'images/hullcity.png');
-
+insert into a_club values (null,'Queens Park Rangers','1882-00-00','Loftus Road','Henry Redknapp',null,'images/queensparkrangers.png','Hammersmith,England');
+insert into a_club values (null,'Leicester City','1884-00-00','King Power Stadium','Nigel Graham Pearson',null,'images/leicestercity.png','Leicester City,England');
+insert into a_club values (null,'Burnley','1882-00-00','Turf Moor','Sean Dyche',null,'images/burnley.png','Burnley,England');
 #更新字符集
 #ALTER DATABASE `aodci` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 #show variables like "%char%";
